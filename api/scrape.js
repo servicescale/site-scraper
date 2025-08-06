c63945b1e06918acc5853b5fc482546f6b10c574
Pat Fong <info@servicescale.com.au>
2025-08-06 15:16:07 +1000

    Test deployment: return static message

--- api/scrape.js
+++ api/scrape.js
@@ -1,18 +1,4 @@
-import * as cheerio from 'cheerio';
 
 export default async function handler(req, res) {
-  const url = req.query.url;
-  if (!url || !/^https?:\/\//i.test(url)) {
-    return res.status(400).json({ error: 'Invalid URL' });
-  }
-
-  try {
-    const html = await fetch(url).then(r => r.text());
-    const base = new URL(url);
-    const $ = cheerio.load(html);
-
-    // ... rest of the logic (see previous message)
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  res.status(200).json({ message: "✅ Scraper is deployed and working" });
 }
